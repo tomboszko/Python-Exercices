d7891c86601e2ba31cdd3915ad24bd3a34e9a8e2
Thomas Gomree <gomreethomas@gmail.com>
2024-01-26 14:35:52 +0100

    Add calculations and unit tests

--- app.py
+++ app.py
@@ -50,8 +50,44 @@ print("The minimum value of the list is: ", minValue)
 maxValue=max(num)
 print("The maximum value of the list is: ", maxValue)
 
+calc="1 + 2"
+result = eval(calc)
+print("The result of the calculation is: ", result)
+stringInterpret=result
+print("The result of the calculation is: ", stringInterpret, type(stringInterpret))
 
-
+import unittest
+ 
+class TestNotebook(unittest.TestCase):
+ 
+    def test_countAlpha(self):
+        self.assertEqual(countAlpha, 12)
+    
+    def test_countFloat(self):
+        self.assertEqual(type(countFloat), type(float()))
+        
+    def test_pi(self):
+        self.assertEqual(3.14, roundPi)
+    
+    def test_reversed(self):
+        self.assertEqual(reversedText, ['!', ' ', 'd', 'l', 'r', 'o', 'w', ' ', 'o', 'l', 'l', 'e', 'H'])
+    
+    def test_age(self):
+        self.assertEqual(type(age), type(str()))
+        
+    def test_sorted(self):
+        self.assertEqual(sortNum, [1, 2, 3, 4, 6, 7, 8])
+    
+    def test_sum(self):
+        self.assertEqual(sumOfList, 31)
+    
+    def test_min(self):
+        self.assertEqual(minValue, 1)
+    
+    def test_max(self):
+        self.assertEqual(maxValue, 8)
+    
+unittest.main(argv=[''], verbosity=2, exit=False)
 if __name__ == "__main__":
     app.run(debug=True)
 
