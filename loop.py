74888484ecdf01825286a8fc385422cfb2fb68b4
Thomas Gomree <gomreethomas@gmail.com>
2024-01-26 14:55:52 +0100

    Refactor variable names for clarity and consistency

---
+++ loop.py
@@ -0,0 +1,10 @@
+while True:
+    n = int(input("give a integer > 0 : "))
+    print("You have provided", n)
+    if n > 0:
+        break
+print("correct answer")
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
