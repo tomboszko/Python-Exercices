50af4f42629f3bf9f2e887851b343d1682adff04
Thomas Gomree <gomreethomas@gmail.com>
2024-01-26 14:29:38 +0100

    Add sorting, sum, min, and max functionality to num list

--- app.py
+++ app.py
@@ -37,12 +37,19 @@ reversedText = list("Hello world!"[::-1])
 print("The reversed string is: ", reversedText)
 
 
-age=int(input("Enter your age: "))
-print("Your age is: ",age, type(age))
+##age=int(input("Enter your age: "))
+##print("Your age is: ",age, type(age))
 
 num = [2, 8, 1, 4, 6, 3, 7] 
 sortNum = sorted(num)
 print("The sorted list is: ", sortNum)
+sumOfList=sum(num)
+print("The sum of the list is: ", sumOfList)
+minValue=min(num)
+print("The minimum value of the list is: ", minValue)
+maxValue=max(num)
+print("The maximum value of the list is: ", maxValue)
+
 
 
 if __name__ == "__main__":
